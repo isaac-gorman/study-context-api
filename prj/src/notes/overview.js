48647b01fc66e9eb86ab4c244289602069fd427a
Isaac Gorman <emailme@isaacg.net>
2020-08-26 11:37:46 -0500

    overview notes completed

---
+++ prj/src/notes/overview.js
@@ -0,0 +1,9 @@
+// Context API Learning Summary
+// - In the typical React app state is passed down parent to child via props, but this prop drilling can become very tedious when many components need those props across the application
+// - With the use of the "Context API" I no longer have conduct prop drilling across large scale applications
+// - Using the Context API I can store the data within a context object, and retrieve that data in the necessary components from the Context API not props
+
+// Module Learning Objectives
+// - Define the problem the context API sloves, and how it solves it
+// - Using a context provider, I will provide data to the component tree
+// - I will need to consume data using the context object from within nested components
